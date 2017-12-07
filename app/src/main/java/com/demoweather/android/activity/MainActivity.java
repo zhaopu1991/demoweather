c76328d312e7b40b7b66067d5ead2b80b742ed12
zhaopu <pu.zhao@gometech.com.cn>
2017-12-07 18:12:25 +0800

    修改文件结构，创建数据库和表

--- app/src/main/java/com/demoweather/android/activity/MainActivity.java
+++ app/src/main/java/com/demoweather/android/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.demoweather.android;
+package com.demoweather.android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.demoweather.android.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/demoweather/android/db/County.java
@@ -0,0 +1,47 @@
+package com.demoweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhaopu on 2017/12/7.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
